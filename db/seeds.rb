0ff809153ae7964cd6489c9fb50626863680e87e
anoorzi <anoorzi123@gmail.com>
2020-03-05 15:24:28 -0500

    add_more_data
    made our dummy data more detailed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,14 @@ user4 = User.create(name: "Mike", age: 40, cc: "8363537",bio: "I love to surf.",
 
 room1 = Room.create( country: "united states",city: "new york city", street: "20-10 53 st", price: "600", bio:"lovely view of the city with a heated pool" )
 room2 = Room.create( country: "united states",city: "los angeles", street: "150-18 75th ave", price: "900", bio: "one bedroom apartment with hot water" )
-room3 = Room.create( country: "canada",city: "toronto", street: "5th ave times square", price: "450", bio:"high tower rise with a downtown view of the city" )
+room3 = Room.create( country: "canada",city: "toronto", street: "5th ave horace st", price: "450", bio:"high tower rise with a downtown view of the city" )
 room4 = Room.create( country: "canada",city: "vancover", street: "19th street 23 ave", price: "1600", bio: "nice little ranch with the horses and chickens" )
 room5 = Room.create( country: "canada",city: "toronto", street: "north 31 street", price: "200", bio:"nice cabin in a ski lounge with a sauna" )
 room6 = Room.create( country: "canada",city: "vancover", street: "southwest ave", price: "700", bio: "an igloo in the ice" )
+room7 = Room.create( country: "japan",city: "tokyo", street: "west 31 st ", price: "978", bio:"right in the heart of tokyo with parking garage" )
+room8 = Room.create( country: "italy ",city: "rome", street: "100-65 20 st", price: "567", bio: "a 5 room 3 bathroom villa" )
+room9 = Room.create( country: "england",city: "london", street: "king george ave 21 st", price: "500", bio:"amazing 16th century victorian mansion" )
+room10 = Room.create( country: "united states",city: "new york city", street: "time square 8th ave", price: "2000", bio: "condo in the heart of manhattan with a pool and gym" )
 
 
     binding.pry
--- run.rb
+++ run.rb
@@ -4,9 +4,9 @@ require_relative "./lib/command_line_interface.rb"
 require "tty-prompt"
 
 current_id = startup
-temp = create_booking(current_id)
+create_checker = create_booking(current_id)
 update_user_bio(current_id)
-delete_me(temp,current_id)
+delete_me(create_checker,current_id)
 
 # "hello"
 binding.pry
